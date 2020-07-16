6e1aca4ad1b058a92f93d5ae767bca7b5aaf41d9
zhengyang <269318650@qq.com>
2020-07-16 21:02:44 +0800

    报表功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Goods from '../components/goods/Goods.vue'
 import Params from '../components/goods/Params.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -29,7 +30,8 @@ const routes = [
       { path: '/goods', component: Goods },
       { path: '/params', component: Params },
       { path: '/goods/add', component: Add },
-      { path: '/orders', component: Order }
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Report }
     ]
   }
 ]
